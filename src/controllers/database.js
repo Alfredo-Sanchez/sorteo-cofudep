88bf2abffa5318c954f78c479215ed4e6e12314d
Alfredo Sanchez <alfredosanchez080114@gmail.com>
2021-03-22 13:05:55 -0300

    dotenv install for conection with db

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,11 +1,15 @@
+//dotenv
+require('dotenv').config()
+
+
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '123',
-    database: 'sorteos',
-    port: '5432'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 module.exports = pool
